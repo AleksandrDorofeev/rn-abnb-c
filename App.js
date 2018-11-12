024cff153f0e80545bd0d9c7fefe61f12a27d9bb
Aleksandr <adorofeev@team.amocrm.com>
2018-11-13 00:42:41 +0300

    done bottom navigation

--- App.js
+++ App.js
@@ -1,41 +1,75 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
+import React, { Component } from "react";
+import { View, Image } from "react-native";
+import { createBottomTabNavigator } from "react-navigation";
 
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from "react-native-vector-icons/Ionicons";
 
-import Explore from './components/screens/Explore';
-import Saved from './components/screens/Saved';
-import Trips from './components/screens/Trips';
-import Inbox from './components/screens/Inbox';
+import Explore from "./components/screens/Explore";
+import Saved from "./components/screens/Saved";
+import Trips from "./components/screens/Trips";
+import Inbox from "./components/screens/Inbox";
+import Profile from "./components/screens/Profile";
 
-class App extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Hello!</Text>
-      </View>
-    )
-  }
-}
-
-export default createBottomTabNavigator({
-  Explore: {
-    screen: Explore,
-    navigationOptions: {
-      tabBarLabel: 'EXPLORE',
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="ios-search" color={tintColor} size={24} />
-      )
+export default createBottomTabNavigator(
+  {
+    Explore: {
+      screen: Explore,
+      navigationOptions: {
+        tabBarLabel: "EXPLORE",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-search" color={tintColor} size={24} />
+        )
+      }
+    },
+    Saved: {
+      screen: Saved,
+      navigationOptions: {
+        tabBarLabel: "SAVED",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-heart-empty" color={tintColor} size={24} />
+        )
+      }
+    },
+    Trips: {
+      screen: Trips,
+      navigationOptions: {
+        tabBarLabel: "EXPLORE",
+        tabBarIcon: ({ tintColor }) => (
+          <Image
+            source={require("./assets/airbnb-icon.png")}
+            style={{ width: 24, height: 24, tintColor: tintColor }}
+          />
+        )
+      }
+    },
+    Inbox: {
+      screen: Inbox,
+      navigationOptions: {
+        tabBarLabel: "INBOX",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-chatboxes" color={tintColor} size={24} />
+        )
+      }
+    },
+    Profile: {
+      screen: Inbox,
+      navigationOptions: {
+        tabBarLabel: "PROFILE",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-person" color={tintColor} size={24} />
+        )
+      }
     }
   },
-  Saved: {
-    screen: Saved
-  },
-  Trips: {
-    screen: Trips
-  },
-  Inbox: {
-    screen: Inbox
+  {
+    tabBarOptions: {
+      activeTintColor: "red",
+      inactiveTintColor: "grey",
+      style: {
+        backgroundColor: "white",
+        borderTopWidth: 0,
+        elevation: 5
+      }
+    }
   }
-});
+);
--- components/screens/Explore.js
+++ components/screens/Explore.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 class Explore extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Explore!</Text>
+        <View style={styles.headerInput}>
+
+        </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  container: {
+    flex: 1
+  },
+  headerInput: {
+    height: 80,
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd'
+  }
+});
 
 export default Explore;
